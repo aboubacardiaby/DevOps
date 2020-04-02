65761d15a49fb8c1b2568f5696b0fceef0bf2e8b
Macmicmac47 <Macmicmac47@users.noreply.github.com>
2020-04-01 19:42:29 -0500

    tes1

---
+++ DevOps/dbo/Tables/MenuItem.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MenuItem]
+(
+	[Id] BIGINT       IDENTITY (1, 1) NOT NULL,
+	[Title] Varchar(70) Not NULL,
+	[Url] Varchar(70) NOT NULL,
+	[OpenInNewWindow ] bit Not NULL,
+	CONSTRAINT [PK_MenuItem_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
