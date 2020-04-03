76d789419e9bb27790781b349fea807c7abfc05c
Macmicmac47 <Macmicmac47@users.noreply.github.com>
2020-04-03 11:25:03 -0500

    add aymptoms

--- DevOps/dbo/Scripts/SymptomData.sql
+++ DevOps/dbo/Scripts/SymptomData.sql
@@ -6,5 +6,7 @@ INSERT [dbo].[Symptoms] ([SymptomId], [ProblemId],[Name],[Description])VALUES(3,
 INSERT [dbo].[Symptoms] ([SymptomId], [ProblemId],[Name],[Description])VALUES(4,1,'Weakness','')
 INSERT [dbo].[Symptoms] ([SymptomId], [ProblemId],[Name],[Description])VALUES(5,1,'Fever','')
 INSERT [dbo].[Symptoms] ([SymptomId], [ProblemId],[Name],[Description])VALUES(6,1,'Night sweats','')
+INSERT [dbo].[Symptoms] ([SymptomId], [ProblemId],[Name],[Description])VALUES(7,1,'Cought sweats','')
+INSERT [dbo].[Symptoms] ([SymptomId], [ProblemId],[Name],[Description])VALUES(8,1,'Loss Appetit sweats','')
 SET IDENTITY_INSERT [dbo].[Symptoms] OFF
 GO
